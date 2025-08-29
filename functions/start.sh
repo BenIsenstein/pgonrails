50041ea9c779061669c8bcdd7ec090f870beaf37
Ben Isenstein <75191941+BenIsenstein@users.noreply.github.com>
2025-08-29 12:10:07 -0600

    fix: edge runtime now supports ipv6

--- functions/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start your legacy service in the background 
-# Start Caddy in the foreground
-
-edge-runtime start --main-service /home/deno/functions/main & \
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
