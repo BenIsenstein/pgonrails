f3804deeb546b262609b7b8087a560c7f3990927
Ben Isenstein <75191941+BenIsenstein@users.noreply.github.com>
2025-09-07 11:14:53 -0600

    feat: env setup script

---
+++ setup-env.sh
@@ -0,0 +1,15 @@
+echo "Creating .env file..."
+
+if [ -f ".env" ]; then
+  echo "Found an existing .env file."
+  exit 1
+fi
+
+if [ ! -f ".env.example" ]; then
+  echo ".env.example file not found. Skipping .env setup..."
+  exit 1
+fi
+
+echo "Copying .env.example to .env..."
+cp .env.example .env
+echo "Setup complete!"
