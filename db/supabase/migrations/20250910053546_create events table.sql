fbb310bea5ed4add16f130a736a30e666414d2a9
Ben Isenstein <75191941+BenIsenstein@users.noreply.github.com>
2025-09-16 10:55:27 -0600

    chore: remove test migrations to public schema

--- db/supabase/migrations/20250910053546_create events table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE events (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-)
--- db/supabase/migrations/20250910162452_add attendees table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.attendees (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    event_id INT REFERENCES public.events(id) ON DELETE CASCADE
-);
-
-ALTER TABLE public.attendees ENABLE ROW LEVEL SECURITY;
