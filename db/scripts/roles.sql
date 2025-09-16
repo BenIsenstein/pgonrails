a7c62181f8720bd02fa6a9055165949daf3fbe42
Ben Isenstein <75191941+BenIsenstein@users.noreply.github.com>
2025-09-15 18:18:18 -0600

    feat: separate passwords for each db role

--- db/scripts/roles.sql
+++ db/scripts/roles.sql
@@ -1,8 +1,12 @@
 -- NOTE: change to your own passwords for production environments
-\set pgpass `echo "$POSTGRES_PASSWORD"`
+\set authenticator_password `echo "$DB_AUTHENTICATOR_PASSWORD"`
+\set pgbouncer_password `echo "$DB_PGBOUNCER_PASSWORD"`
+\set auth_password `echo "$DB_AUTH_PASSWORD"`
+\set webhooks_password `echo "$DB_WEBHOOKS_PASSWORD"`
+\set storage_password `echo "$DB_STORAGE_PASSWORD"`
 
-ALTER USER authenticator WITH PASSWORD :'pgpass';
-ALTER USER pgbouncer WITH PASSWORD :'pgpass';
-ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
+ALTER USER authenticator WITH PASSWORD :'authenticator_password';
+ALTER USER pgbouncer WITH PASSWORD :'pgbouncer_password';
+ALTER USER supabase_auth_admin WITH PASSWORD :'auth_password';
+ALTER USER supabase_functions_admin WITH PASSWORD :'webhooks_password';
+ALTER USER supabase_storage_admin WITH PASSWORD :'storage_password';
